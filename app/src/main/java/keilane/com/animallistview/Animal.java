2870f7e6b6c4673a5acc43b458b5c00f1cc2ccce
Keilane Rizza <keilanerizza@gmail.com>
2018-10-22 16:52:31 -0300

    Commit inicial do projeto

---
+++ app/src/main/java/keilane/com/animallistview/Animal.java
@@ -0,0 +1,11 @@
+package keilane.com.animallistview;
+
+public class Animal {
+    public String nome;
+    public int foto;
+
+    public Animal(String nome, int foto) {
+        this.nome = nome;
+        this.foto = foto;
+    }
+}
